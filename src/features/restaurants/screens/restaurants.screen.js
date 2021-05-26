5c3c117012dc0c6583d1d9c55eaef15554e415ec
Denis Lysionak <dzianislysionak@gmail.com>
2021-05-26 07:46:30 +0300

    basicc implement

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import { FlatList } from 'react-native'
+import { Searchbar } from 'react-native-paper'
+
+import { RestaurantInfoCard } from '../components/restaurant-info-card.component'
+import { Spacer } from '../../../components/spacer/spacer.component'
+import { SafeArea } from '../../../components/utility/safe-area.component'
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``
+
+export const RestaurantsScreen = () => {
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantList
+        data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }, { name: 5 }]}
+        renderItem={() => (
+          <Spacer position='bottom' size='large'>
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
+  )
+}
