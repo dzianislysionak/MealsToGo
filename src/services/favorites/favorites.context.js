486cddc4ce4035b95195f346ad6e8cfe124d38c8
Denis Lysionak <dzianislysionak@gmail.com>
2021-05-27 08:48:56 +0300

    local storage

--- src/services/favorites/favorites.context.js
+++ src/services/favorites/favorites.context.js
@@ -1,9 +1,31 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const FavoritesContext = createContext()
 
 export const FavoritesContextProvider = ({ children }) => {
   const [favorites, setFavoreites] = useState([])
+
+  const saveFavorites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('@favorites', jsonValue)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const loadFavorites = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@favorites')
+      if (value !== null) {
+        console.log('from LS', JSON.parse(value))
+        setFavoreites(JSON.parse(value))
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
   const add = (restaurant) => {
     setFavoreites([...favorites, restaurant])
@@ -16,6 +38,15 @@ export const FavoritesContextProvider = ({ children }) => {
 
     setFavoreites(newFavorites)
   }
+
+  useEffect(() => {
+    saveFavorites(favorites)
+  }, [favorites])
+
+  useEffect(() => {
+    loadFavorites()
+  }, [])
+
   return (
     <FavoritesContext.Provider
       value={{ favorites, addToFavorites: add, removeFromFavorites: remove }}
